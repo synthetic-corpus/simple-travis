e3bbefec1a1a9ef200e89069cfc9b49d0d4d0cf8
synthetic.corpus <joel@joelgonzaga.com>
2020-09-03 16:08:58 -0700

    Set to user environment variable now too.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const environment = process.env.SUPER_VARIABLE || "Running Locally"
+
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`Containers rule! And the Variable is ${environment}`);
     await sleep(5000);
   }
 }
